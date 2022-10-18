a96043690b39a381cc1b2c0ba6298546992bdbaa
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-10-18 17:52:58 -0500

    Add felicias changes to dev and init commands

--- cli/src/commands/dev.js
+++ cli/src/commands/dev.js
@@ -4,13 +4,7 @@ import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
 import { meshDev } from "../utils/helpers.js";
 
-console.log(
-  chalk.red(
-    figlet.textSync("Let's Run This Mesh", { horizontalLayout: "full" })
-  )
-);
-
-export default async () => {
+const dev = async () => {
   await inquirer.prompt([
     {
       name: "confirmRunDev",
@@ -20,11 +14,21 @@ export default async () => {
   ]);
 
   const startServer = () => {
-    let spinner = createSpinner("Start your server.\n").start();
+    let spinner = createSpinner("Starting your server...\n").start();
 
     meshDev();
     spinner.success({ text: "Server started on Port 4000." });
   };
 
   startServer();
+};
+
+export default async () => {
+  console.log(
+    chalk.red(
+      figlet.textSync("Let's Run This Mesh", { horizontalLayout: "full" })
+    )
+  );
+
+  await dev();
 };
