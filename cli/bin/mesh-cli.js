151537a3d66ab9b592b533c7f54fca601de96af1
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-10-19 12:05:18 -0500

    Add basic team4 add command to add second data source, without functionality yet

--- cli/bin/mesh-cli.js
+++ cli/bin/mesh-cli.js
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import init from '../src/commands/init.js';
-import dev from '../src/commands/dev.js';
+import { program } from "commander";
+import init from "../src/commands/init.js";
+import dev from "../src/commands/dev.js";
+import add from "../src/commands/add.js";
 
 program
-  .command('init')
-  .alias('i')
-  .description('Initialize new project with .meshrc.yaml configuration file')
-  .action(init)
+  .command("init")
+  .alias("i")
+  .description("Initialize new project with .meshrc.yaml configuration file")
+  .action(init);
 
 program
-  .command('dev')
-  .alias('d')
-  .description('Starts a development graphql mesh server')
-  .action(dev)
+  .command("dev")
+  .alias("d")
+  .description("Starts a development graphql mesh server")
+  .action(dev);
+
+program
+  .command("add source")
+  .alias("a")
+  .description("Adds a new data source to your graphql server")
+  .action(add);
 
 program.parse(process.argv);
---
+++ cli/src/commands/add.js
@@ -0,0 +1,92 @@
+import chalk from "chalk";
+import figlet from "figlet";
+import inquirer from "inquirer";
+
+// import {
+//   createMeshConfig,
+//   initProject,
+//   installHandlers,
+// } from "../utils/helpers.js";
+
+const add = async () => {
+  let answers = await inquirer.prompt([
+    {
+      name: "dataSourceType",
+      type: "list",
+      message: "Why type of data source would you like to add?",
+      choices: ["postgres", "graphql"],
+      default: 0,
+    },
+    {
+      name: "name",
+      type: "input",
+      message: "Enter a name for this data source:",
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return "Please enter a name for this data source";
+        }
+      },
+    },
+    {
+      name: "pg-url",
+      type: "input",
+      message: "Enter your postgres connection string:",
+      when: (answer) => answer.dataSourceType === "postgres",
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return "Please enter a postgres connection string";
+        }
+      },
+    },
+    {
+      name: "gql-endpoint",
+      type: "input",
+      message: "Enter your graphql endpoint for this data source:",
+      when: (answer) => answer.dataSourceType === "graphql",
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return "Please enter a graphql endpoint";
+        }
+      },
+    },
+    {
+      name: "confirm-data-source",
+      type: "confirm",
+      message: "Are you ready to add this data source?",
+    },
+  ]);
+
+  console.log(`Successfully added your ${answers.dataSourceType} data source!`);
+  // const init = () => {
+  //   let spinner = createSpinner("Initializing your project folder.\n").start();
+
+  //   initProject();
+  //   spinner.success({ text: "Project folder has been initialized." });
+
+  //   spinner = createSpinner("Installing mesh handlers.\n").start();
+
+  //   installHandlers();
+  //   spinner.success({ text: "Handlers installed." });
+
+  //   spinner = createSpinner("Generating mesh server.\n").start();
+
+  //   createMeshConfig(input.name, input.connectionString);
+  //   spinner.success({ text: "Mesh server created" });
+  // };
+
+  // init();
+};
+
+export default async () => {
+  console.log(
+    chalk.red(figlet.textSync("New Data Source", { horizontalLayout: "full" }))
+  );
+
+  add();
+};
