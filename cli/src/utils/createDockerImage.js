fe888c2c6164d89a9a30f2d68c7b8dcced1b306f
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-10-24 16:15:22 -0500

    Add stdio:inherit to createDockerImage.js in utils

--- cli/src/utils/createDockerImage.js
+++ cli/src/utils/createDockerImage.js
@@ -1,5 +1,8 @@
 import { execSync } from "child_process";
 
 export default () => {
-  execSync("docker build --platform linux/amd64 -t nexus-image .", { encoding: "utf-8" });
+  execSync("docker build --platform linux/amd64 -t nexus-image .", {
+    stdio: "inherit",
+    encoding: "utf-8",
+  });
 };
