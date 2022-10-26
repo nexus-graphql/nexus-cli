cbf12d3fd4d3e93ce845ec26fe596feef247b92e
Kim Ramirez <kim.ram27@gmail.com>
2022-10-26 17:35:20 -0600

    added ecs folder again

--- cli/src/configTemplates/deployment/AWS/main.tf
+++ cli/src/configTemplates/deployment/AWS/main.tf
@@ -6,3 +6,13 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
   shared_config_files = ["~/.aws/config"]
 }
+
+module "ecr" {
+  source = "./ecr"
+}
+
+module "ecs" {
+  source = "./ecs"
+  aws_region = var.aws_region
+  respository_url = module.ecr.aws_ecr_repository
+}
