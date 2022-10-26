01b54c33b9e4d0206437363e95b4cb712393c226
Kim Ramirez <kim.ram27@gmail.com>
2022-10-26 17:26:32 -0600

    added ecr folder

---
+++ cli/src/utils/deployTerraform.js
@@ -0,0 +1,19 @@
+import { execSync } from "child_process";
+import pushToERC from "./pushToERC.js";
+
+export default async () => {
+  execSync("terraform init -target=module.ecr", {
+    stdio: "inherit",
+    encoding: "utf-8",
+  });
+  execSync("terraform plan -target=module.ecr", {
+    stdio: "inherit",
+    encoding: "utf-8",
+  });
+  execSync("terraform apply -target=module.ecr", {
+    stdio: "inherit",
+    encoding: "utf-8",
+  });
+
+  pushToERC();
+};
