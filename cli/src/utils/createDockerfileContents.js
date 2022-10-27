107c09bf63d9bd988da1100af86264588240b3bf
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-10-27 14:41:01 -0500

    Dockerfile generator copies all files over to dockerfile

--- cli/src/utils/createDockerfileContents.js
+++ cli/src/utils/createDockerfileContents.js
@@ -1,10 +1,9 @@
-export default port => `FROM node:18
+export default (port) => `FROM node:18
 WORKDIR /usr/src/server
 COPY package*.json ./
-COPY .meshrc.yaml ./
+COPY . .
 RUN npm i
 RUN npx mesh build
 EXPOSE ${port}
 CMD [ "npx", "mesh", "start"]
 `;
-
