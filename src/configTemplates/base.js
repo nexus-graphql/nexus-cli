74f75a29df417c31bc409d0855334f4b7c0922b3
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-11-11 16:31:00 -0600

    Add additionaltypedefs and additional resolvers to base template

--- src/configTemplates/base.js
+++ src/configTemplates/base.js
@@ -1,1 +1,35 @@
-export default {"sources":[{"name":null,"handler":{"postgraphile":{"connectionString":null,"options":{"simpleCollections":"only"},"appendPlugins":["postgraphile-plugin-connection-filter"]}}}],"transforms":[{"resolversComposition":{"mode":"wrap","compositions":[{"resolver":"Query.*","composer":"./src/composers/isAuth.js#isAuth"},{"resolver":"Mutation.*","composer":"./src/composers/isAuth.js#isAuth"},{"resolver":"Subscription.*","composer":"./src/composers/isAuth.js#isAuth"}]}}],"serve":{"browser":false,"playgroundTitle":"Nexus"}}
+export default {
+  sources: [
+    {
+      name: null,
+      handler: {
+        postgraphile: {
+          connectionString: null,
+          options: { simpleCollections: "only" },
+          appendPlugins: ["postgraphile-plugin-connection-filter"],
+        },
+      },
+    },
+  ],
+  transforms: [
+    {
+      resolversComposition: {
+        mode: "wrap",
+        compositions: [
+          { resolver: "Query.*", composer: "./src/composers/isAuth.js#isAuth" },
+          {
+            resolver: "Mutation.*",
+            composer: "./src/composers/isAuth.js#isAuth",
+          },
+          {
+            resolver: "Subscription.*",
+            composer: "./src/composers/isAuth.js#isAuth",
+          },
+        ],
+      },
+    },
+  ],
+  additionalResolvers: null,
+  additionalTypeDefs: null,
+  serve: { browser: false, playgroundTitle: "Nexus" },
+};
